f5be72feea99aba9b65ec769a016a1f8ab04fe21
Rafael Reis <103606350+devRafaReis@users.noreply.github.com>
2022-09-15 10:19:32 -0300

    Add files via upload
    Commit inicial

---
+++ src/components/TarefaLista.vue
@@ -0,0 +1,32 @@
+<template>
+    <BoxTarefa>
+        <div class="columns">
+            <div class="column is-7">{{tarefa.descricao || 'Tarefa sem descrição'}}</div>
+            <div class="column">
+                <CronometroPrincipal :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+            </div>
+        </div>
+    </BoxTarefa>
+</template>
+
+<script lang="ts">
+import ITarefa from "@/interfaces/ITarefa";
+import { defineComponent, PropType } from "vue";
+import CronometroPrincipal from "./CronometroPrincipal.vue";
+import BoxTarefa from "./BoxTarefa.vue";
+
+export default defineComponent({
+   name: "TarefaLista",
+   components: {
+    CronometroPrincipal,
+    BoxTarefa
+},
+    props: {
+        tarefa: {
+            type: Object as PropType<ITarefa>,
+            required: true
+        }
+    }
+});
+</script>
+
